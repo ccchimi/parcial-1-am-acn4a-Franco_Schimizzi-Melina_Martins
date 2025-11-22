d0b3094175faaadc9d7df91268154b9e00ac7977
Franco Schimizzi <franco.schimizzi@davinci.edu.ar>
2025-11-21 23:14:18 -0300

    fix (api):
    actualizar url de recetas al renombrar repositorio

--- app/src/main/java/com/app/tasteit/RetrofitClient.java
+++ app/src/main/java/com/app/tasteit/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     private static final String BASE_URL =
-            "https://raw.githubusercontent.com/ccchimi/parcial-2-am-acn4a-Franco_Schimizzi-Melina_Martins/main/";
+            "https://raw.githubusercontent.com/ccchimi/parcial-2-am-acn4a-Schimizzi-Martins/main/";
 
     private static Retrofit retrofit = null;
 
